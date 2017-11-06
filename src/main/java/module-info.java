61fecb39e1de0bdf041731a57e932a207ca90088
Gaël Lalire <gael.lalire@laposte.net>
2017-11-07 00:32:33 +0100

    import

---
+++ src/main/java/module-info.java
@@ -0,0 +1,25 @@
+module fr.gaellalire.vestige.maven {
+    
+    requires org.apache.maven.resolver;
+
+    requires org.apache.maven.resolver.impl;
+
+    requires org.apache.maven.resolver.spi;
+
+    requires org.apache.maven.resolver.util;
+
+    requires java.sql;
+
+    exports org.apache.maven.settings.building;
+
+    exports org.apache.maven.model;
+    
+    exports org.apache.maven.model.building;
+    
+    exports org.apache.maven.model.superpom;
+    
+    exports org.apache.maven.repository.internal;
+    
+    exports org.apache.maven.settings;
+    
+}
